ecb495c95cf927c023dbca3058783087d65ddb45
clehider <eduarddring@gmail.com>
2025-02-13 11:10:11 +0000

    Pending changes exported from your codespace

--- src/components/GestionVehiculos.tsx
+++ src/components/GestionVehiculos.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  React, { useState, useEffect } from "react";
 import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebase/config";
 
